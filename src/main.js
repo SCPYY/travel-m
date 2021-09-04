e104662c1be925074ef4bce857cbd495787f2c05
SCPYY <1621850997@qq.com>
2021-09-04 15:18:54 +0800

    secend commit

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+//导入全局样式
+import './style/index.less'
+
+//全局导入vant组件
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+import 'amfe-flexible'
+
+createApp(App).use(store).use(router).use(Vant).mount('#app')
