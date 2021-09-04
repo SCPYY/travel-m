50f4bce82cce1580896cccd25dd6e78244be842c
SCPYY <1621850997@qq.com>
2021-09-04 16:53:34 +0800

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="nav">
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="success">成功按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <i class="iconfont travel-kaixin"></i>
+    <!-- 路由出口 -->
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+export default {
+    name : "App"
+}
+</script>
 
 <style lang="less">
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,32 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
+  {
+    path : '/login',
+    name : 'login',
+    component : () => import('@/views/login')
+  },
+  {
+    path : '/',
+    component : () => import('@/views/layout'),
+    children : [
+      {
+        path : '',   //默认子路由
+        name : 'home',
+        component : () => import('@/views/home/')
+      },
+      {
+        path : '/travel',
+        name : 'travel',
+        component : () => import('@/views/travel/')
+      },
+      {
+        path : '/mine',   //默认子路由
+        name : 'mine',
+        component : () => import('@/views/mine/')
+      }
+    ]
+  }
 ]
 
 const router = createRouter({
